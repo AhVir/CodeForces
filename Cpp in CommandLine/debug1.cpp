74f41fd49feafad1e71df92ec41f4fc1bec30ce7
AhVir <shkhtanvirahmed@gmail.com>
2024-03-20 09:41:39 +0600

    ,

---
+++ Cpp in CommandLine/debug1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include "debugging.h"
+using namespace std;
+
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+
+int getValue(){
+  return 4;
+}
+
+int main(){
+//  cout << getValue << endl;
+
+  int x; x = 3;
+
+  dbg(x);
+
+  cout << x*3 << endl;
+
+  dbg(x);
+
+  cout << x << endl;
+
+  return 0;
+}
---
+++ Cpp in CommandLine/name.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+  int x; cin >> x;
+  cout << "Found: " << x << "\n";
+
+  int ar[5];
+  cout << ar[-1] << endl;
+
+  return 0;
+} 
